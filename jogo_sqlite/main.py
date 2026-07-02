b0c1dcfd48b6174e2d30ad0fbb3c7f60ac5a2bd6
Lucas <lululucas2507@gmail.com>
2026-07-02 14:23:36 -0300

    Adição da função ganhar_ouro() e buscar_personagem_por_id()

--- jogo_sqlite/main.py
+++ jogo_sqlite/main.py
@@ -57,10 +57,60 @@ def criar_personagem():
     conexao.commit()
     conexao.close()
 
+# Buscar personagem por ID
+def buscar_personagem_por_id():
+    id_personagem = int(input("Digite o ID do personagem: "))
+    
+    conexao = conectar ()
+    cursor = conexao.cursor()
+
+    cursor.execute("""  
+    SELECT * FROM personagens 
+    WHERE id = ?
+    """, (id_personagem,))
+
+    personagem = cursor.fetchone()
+
+    if personagem: 
+        print("\n-- PERSONAGEM ENCONTRADO --")
+        print(f'ID: {personagem[0]}')
+        print(f'Nome: {personagem[1]}')
+        print(f'Classe: {personagem[2]}')
+        print(f'Vida: {personagem[3]}')
+        print(f'Ouro: {personagem[4]}')
+        print("--------------------------------- \n")
+    else:
+        print("Personagem não encontrado.")
+    
+    conexao.close()
+
+def ganhar_ouro():
+    id_personagem = int(input("Digite o ID do personagem: "))
+    quantidade_ouro = int(input("Digite a quantidade de ouro a ser adicionada: "))
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+                   UPDATE personagens
+                   SET ouro = ouro + ?
+                   WHERE id = ?""",
+                   (quantidade_ouro, id_personagem))
+    
+    if cursor.rowcount > 0:
+        print(f"{quantidade_ouro} de ouro adicionado ao personagem com ID {id_personagem}.")
+    else:
+        print("Personagem não encontrado.")
+
+    conexao.commit()
+    conexao.close()
+    
 def mostrar_menu():
-        print(""" ========== MENU DO JOGO ==========
+        print("""\n ========== MENU DO JOGO ==========
 1 - Listar personagens 
 2 - Criar personagem 
+3 - Buscar personagem por ID
+4 - Ganhar ouro
 0 - Sair 
 =================================== """) 
 
@@ -74,6 +124,10 @@ def executar_programa():
             listar_personagens()
         elif opcao == "2":
             criar_personagem()
+        elif opcao == "3":
+            buscar_personagem_por_id()
+        elif opcao == "4":
+            ganhar_ouro()
         elif opcao == "0":
             print("Saindo do jogo...")
             break
